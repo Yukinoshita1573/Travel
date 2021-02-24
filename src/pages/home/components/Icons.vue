ea431d70184876a60f890b5eda46ca246bc43bab
Yukinoshita1573 <2272614907@qq.com>
2021-02-24 13:14:33 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="icons">
+      <swiper>
+          <swiper-slide v-for="(page,index) in pages" :key="index">
+              <div class="icon" v-for="item in page" :key="item.id">
+                <div class="icon-img">
+                    <img class="icon-img-content" :src="item.imgUrl" alt="">
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+              </div>
+          </swiper-slide>
+      </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'HomeIcons',
+  data(){
+      return {
+          iconList:[
+              {
+                  id:'0001',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'景点门票'
+              },
+              {
+                  id:'0002',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'滑雪季'
+              },
+              {
+                  id:'0003',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'泡温泉'
+              },
+              {
+                  id:'0004',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'动植物园'
+              },
+              {
+                  id:'0005',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'一日游'
+              },
+              {
+                  id:'0006',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'必游榜单'
+              },
+              {
+                  id:'0007',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'自然风光'
+              },
+              {
+                  id:'0008',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'全部'
+              },
+              {
+                  id:'0009',
+                  imgUrl:'//img1.qunarzz.com/sight/p0/1807/7d/7d64de6b134d10eaa3.img.jpg_600x330_0297e8aa.jpg',
+                  desc:'全部'
+              }
+          ]
+      }
+  },
+  computed:{
+      pages(){
+          const pages = []
+          this.iconList.forEach((item,index)=>{
+              const page = Math.floor(index / 8)
+              if(!pages[page]){
+                  pages[page] = []
+              }
+              pages[page].push(item)
+          })
+          return pages
+      }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+ @import '~styles/mixins.styl'
+    .icons >>> .swiper-container
+        height: 0
+        padding-bottom: 50%
+    .icon  
+        position: relative
+        overflow: hidden
+        float: left
+        width: 25%
+        height: 0
+        padding-bottom: 25%
+        .icon-img
+            position: absolute
+            top: 0
+            left: 0
+            right: 0
+            bottom: .44rem
+            box-sizing: border-box
+            padding: .1rem
+            .icon-img-content
+                height: 70%
+                display: block
+                margin: 0 auto
+        .icon-desc 
+            position: absolute
+            left: 0
+            right: 0
+            bottom: 0
+            height: .44rem
+            line-height: 0.44rem
+            text-align: center
+            color: $darkTextColor
+            ellipsis()
+</style>
