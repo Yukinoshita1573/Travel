3410d860f865885249cc0d729aa378ed575fbd03
Yukinoshita1573 <2272614907@qq.com>
2021-02-25 14:08:23 +0800

    finish pages

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import { mapState } from 'vuex'
 
 import axios from 'axios'
 
@@ -29,15 +30,22 @@ export default {
   data(){
       return {
         //   city:'',
+          lastCity:'',
           swiperList:[],
           iconList:[],
           recommendList:[],
           weekendList:[]
       }
   },
+  computed:{
+     ...mapState({
+       city:'city'
+     })
+    //  ...mapState(['city'])
+  },
   methods:{
       getHomeInfo(){
-          axios.get('api/index.json').then(this.getHomeInfoSucc)
+          axios.get('api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
       
       },
       getHomeInfoSucc(res){
@@ -54,7 +62,14 @@ export default {
       }
   },
   mounted(){
+    this.lastCity = this.city
       this.getHomeInfo()
+  },
+  activated(){
+    if(this.lastCity !== this.city){
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
